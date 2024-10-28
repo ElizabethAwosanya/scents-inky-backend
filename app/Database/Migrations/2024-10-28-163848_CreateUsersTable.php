ae96d8666551303fb798884c77a6e959ed9c6ce0
Elizabeth Awosanya <e.e.awosanya@wlv.ac.uk>
2024-10-28 17:25:23 +0000

    feat: user table migrations

--- app/Database/Migrations/2024-10-28-163848_CreateUsersTable.php
+++ app/Database/Migrations/2024-10-28-163848_CreateUsersTable.php
@@ -8,11 +8,44 @@ class CreateUsersTable extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'username' => [
+                'type' => 'VARCHAR',
+                'constraint' => 50,
+            ],
+            'email' => [
+                'type' => 'VARCHAR',
+                'constraint' => 100,
+            ],
+            'password' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+
+        // Set primary key
+        $this->forge->addKey('id', true);
+        // Create the table
+        $this->forge->createTable('users');
     }
 
     public function down()
     {
-        //
+        // Drop the table if it exists
+        $this->forge->dropTable('users');
     }
 }
