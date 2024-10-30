e17a10f13cc12cab8710097143b44221d5298e0a
Elizabeth Awosanya <e.e.awosanya@wlv.ac.uk>
2024-10-30 02:39:18 +0000

    chore: modify api routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,29 +8,8 @@ use CodeIgniter\Router\RouteCollection;
 
 $routes->get('/', 'Home::index');
 
-// User Routes
-$routes->group('users', ['namespace' => 'App\Controllers'], function($routes) {
-    $routes->get('/', 'UserController::index');         // Get all users
-    $routes->get('(:num)', 'UserController::show/$1');  // Get a single user by ID
-    $routes->post('/', 'UserController::create');       // Create a new user
-    $routes->put('(:num)', 'UserController::update/$1');// Update an existing user by ID
-    $routes->delete('(:num)', 'UserController::delete/$1'); // Delete a user by ID
-});
-
-// Order Routes
-$routes->group('orders', ['namespace' => 'App\Controllers'], function($routes) {
-    $routes->get('/', 'OrderController::index');          // Get all orders
-    $routes->get('(:num)', 'OrderController::show/$1');   // Get a single order by ID
-    $routes->post('/', 'OrderController::create');        // Create a new order
-    $routes->put('(:num)', 'OrderController::update/$1'); // Update an existing order by ID
-    $routes->delete('(:num)', 'OrderController::delete/$1'); // Delete an order by ID
-});
-
-// Product Routes
-$routes->group('products', ['namespace' => 'App\Controllers'], function($routes) {
-    $routes->get('/', 'ProductsController::index');           // Get all products
-    $routes->get('(:num)', 'ProductsController::show/$1');    // Get a single product by ID
-    $routes->post('/', 'ProductsController::create');         // Create a new product
-    $routes->put('(:num)', 'ProductsController::update/$1');  // Update an existing product by ID
-    $routes->delete('(:num)', 'ProductsController::delete/$1'); // Delete a product by ID
-});
+$routes->get('/products', 'ProductsController::index');
+$routes->get('/products/(:num)', 'ProductsController::show/$1');
+$routes->post('/products', 'ProductsController::create');
+$routes->put('/products/(:num)', 'ProductsController::update/$1');
+$routes->delete('/products/(:num)', 'ProductsController::delete/$1');
